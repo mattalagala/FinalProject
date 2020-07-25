efba87a266a91f548ed55b1e5d15d2ea0744027d
mattalagala <matt15y@gmail.com>
2020-07-25 16:38:21 -0500

    updated google oauth creds

--- passport.js
+++ passport.js
@@ -3,25 +3,25 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 
 //Passport.js setup
-passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-  
-  passport.deserializeUser(function(user, done) {
-    // User.findById(id, function(err, user) {
-      done(null, user);
-    // });
-  });
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  // User.findById(id, function(err, user) {
+  done(null, user);
+  // });
+});
 
 passport.use(new GoogleStrategy({
-    clientID: '214399234969-3s3pb55rg0r46bhleosqddnc8fs57ucm.apps.googleusercontent.com',
-    clientSecret: 'iP6SceV9Rabw4jV8YugjrbBR',
-    callbackURL: "http://localhost:9999/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
+  clientID: '214399234969-3s3pb55rg0r46bhleosqddnc8fs57ucm.apps.googleusercontent.com',
+  clientSecret: 'iP6SceV9Rabw4jV8YugjrbBR',
+  callbackURL: "https://northrange.dev/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
     // user profile info (mainly profile id) to check if the user is registered in your db. 
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(null, profile);
+    return done(null, profile);
     // });
   }
 ));
